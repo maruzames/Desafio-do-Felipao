3a7aa8d3053b15bb83f782582fa02fa99366a9ec
maruzames <maruzames@gmail.com>
2024-06-14 23:31:42 -0300

    desafio -1

---
+++ index.js
@@ -0,0 +1,26 @@
+let heroi = "maruzames"
+let xp = 0
+
+if(xp <=1000){
+        console.log("o Herói " + heroi + " está no nível Ferro")
+ } 
+    else if(xp >1000 && xp <=2000){
+        console.log("o Herói " + heroi + " está no nível Bronze")
+    }
+    else if(xp >2000 && xp <= 5000){
+        console.log("o Herói " + heroi + " está no nível Prata")
+    }
+    else if(xp >5000 && xp <= 7000){
+        console.log("o Herói " + heroi + " está no nível Ouro")
+    }
+    else if(xp >7000 && xp <= 8000){
+        console.log("o Herói " + heroi + " está no nível Platina")
+    }
+    else if(xp >8000 && xp <=9000){
+        console.log("o Herói " + heroi + " está no nível Ascendente")
+    }
+    else if(xp >9000 && xp <=10000){
+        console.log("o Herói " + heroi + " está no nível Imortal")
+    }
+    else
+        (console.log("o Herói " + heroi + " está no nível Radiante"))
